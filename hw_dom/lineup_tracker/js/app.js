77e8833539126c79e571c45aa0e859f5a5467da5
Becky <rebeccajoynielsen@gmail.come>
2019-04-07 16:36:53 +0100

    adds star pass button to each team

--- hw_dom/lineup_tracker/js/app.js
+++ hw_dom/lineup_tracker/js/app.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //creating new skater
     const newSkater = document.createElement('li');
     newSkater.classList.add(newSkaterClass);
+    newSkater.classList.add(newSkaterTeam);
     newSkater.appendChild(newSkaterNum);
     newSkater.appendChild(newSkaterName);
 
@@ -37,9 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
     team1.appendChild(newSkater);
   } else {
     const team2 = document.querySelector('ul.team2');
-    console.log('trying to add player to team 2');
     team2.appendChild(newSkater);
   };
+
 
 
     form.reset();
@@ -55,13 +56,44 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('all skaters deleted');
 
     const team1List = document.querySelector('ul.team1');
-    team1List.innerHTML = "<h2>team one</h2>";
+    team1List.innerHTML = "<h2>team one</h2> <button type=\"button\" id=\"team1-starpass\">star pass</button>";
     ;
 
     const team2List = document.querySelector('ul.team2');
-    team2List.innerHTML = "<h2>team two</h2>";
+    team2List.innerHTML = "<h2>team two</h2> <button type=\"button\" id=\"team2-starpass\">star pass</button>";
   };
 
   deleteAll.addEventListener('click', handleDeleteAll);
 
+  // star pass
+
+  const team1StarPass = document.querySelector('#team1-starpass');
+
+    const handleStarPass1 = function(evt){
+      console.log('team 1 star pass');
+
+      const team1Jammer = document.querySelector('li.jammer.team1')
+      team1Jammer.classList.replace('jammer', 'blocker')
+
+      const team1Pivot = document.querySelector('li.pivot.team1');
+      team1Pivot.classList.replace('pivot', 'jammer');
+    };
+
+  team1StarPass.addEventListener('click', handleStarPass1);
+
+  // star pass team 2
+
+  const team2StarPass = document.querySelector('#team2-starpass');
+
+    const handleStarPass2 = function(evt){
+      console.log('team 2 star pass');
+
+      const team2Jammer = document.querySelector('li.jammer.team2')
+      team2Jammer.classList.replace('jammer', 'blocker')
+
+      const team2Pivot = document.querySelector('li.pivot.team2');
+      team2Pivot.classList.replace('pivot', 'jammer');
+    };
+
+  team2StarPass.addEventListener('click', handleStarPass2);
 });
